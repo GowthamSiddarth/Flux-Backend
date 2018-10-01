57410b1eb73a4e37a8fc632adf4e2ac6f0de6def
Gowtham Behara <gothsiddu@gmail.com>
2018-10-01 14:35:32 +0530

    Add attributes to UserSchema

--- src/users/user.model.js
+++ src/users/user.model.js
@@ -4,8 +4,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: String,
-    password: String,
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
 });
+
+UserSchema.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model('User', UserSchema);
